085cdb9b51762574487288fea8f71f0d9579f692
Jainam Shah <jainam.shah2@cygnet.one>
2025-02-15 20:41:03 +0530

    Initial Commit

---
+++ child-service/src/test/java/com/jdshah/ChildServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdshah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ grandchild-service/src/main/java/com/jdshah/GrandchildServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jdshah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandchildServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandchildServiceApplication.class, args);
+	}
+
+}
